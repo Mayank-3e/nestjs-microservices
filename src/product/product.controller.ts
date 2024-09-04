0e0edd81cb8516c3264900660ad41fbace7b3cb4
Mayank Agarwal <7mayank.agarwal@gmail.com>
2024-09-04 15:39:13 +0530

    complete Products CRUD

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { ProductService } from './product.service';
 
 @Controller('products')
@@ -8,5 +8,32 @@ export class ProductController {
   @Get()
   all() {
     return this.productService.all()
+  }
+
+  @Post()
+  create(
+    @Body('title') title: string,
+    @Body('image') image: string
+  ) {
+    return this.productService.create(title,image)
+  }
+
+  @Get(':id')
+  get(@Param('id') id: number) {
+    return this.productService.get(id)
+  }
+
+  @Put(':id')
+  update(
+    @Param('id') id: number,
+    @Body('title') title: string,
+    @Body('image') image: string
+  ) {
+    return this.productService.update(id,title,image)
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return this.productService.delete(id)
   }
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -12,4 +12,20 @@ export class ProductService {
   async all(): Promise<Product[]> {
     return await this.productRepo.find()
   }
+
+  async create(title: string,image: string): Promise<Product> {
+    return await this.productRepo.save({ title, image })
+  }
+
+  async get(id: number): Promise<Product> {
+    return await this.productRepo.findOneBy({id})
+  }
+
+  async update(id: number,title: string,image: string) {
+    return await this.productRepo.update(id, { title, image })
+  }
+
+  async delete(id: number) {
+    return await this.productRepo.delete(id)
+  }
 }
